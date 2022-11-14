60d82c98c5a1379555129dcdcc16c46dc3750ea4
Pournami Varma <114948406+pournamivarma@users.noreply.github.com>
2022-11-14 11:35:37 -0800

    Create Chart 1.R

---
+++ source/Chart 1.R
@@ -0,0 +1,8 @@
+# Chart 1 - Visualization of the sustainability scores for over 15K corporates.
+
+# loading ESG Ratings and Companies by Industry Data 
+library(tidyverse)
+library(ggplot2)
+
+data <- read_csv('https://media.githubusercontent.com/media/info201a-au2022/project-group-8-section-af/main/data/sustainability_scores.csv')
+View(data)
