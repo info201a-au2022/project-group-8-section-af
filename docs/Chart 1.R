26f6855e75a970ce12ea629574083c748055c823
Pournami Varma <114948406+pournamivarma@users.noreply.github.com>
2022-11-14 11:35:19 -0800

    Delete Chart 1.R

--- docs/Chart 1.R
+++
@@ -1,9 +0,0 @@
-# Chart 1 - Visualization of the sustainability scores for over 15K corporates.
-
-# loading ESG Ratings and Companies by Industry Data 
-library(tidyverse)
-library(ggplot2)
-
-data <- read_csv('https://media.githubusercontent.com/media/info201a-au2022/project-group-8-section-af/main/data/sustainability_scores.csv')
-View(data)
-
