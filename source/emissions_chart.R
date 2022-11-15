c584e2ac23d87c137c19b885491f694c273f1d38
ciaram14 <114948212+ciaram14@users.noreply.github.com>
2022-11-15 00:51:01 -0800

    Update emissions_chart.R

--- source/emissions_chart.R
+++ source/emissions_chart.R
@@ -12,3 +12,7 @@ top_30_emissions <- select(Country, CO2.emissions..latest.year,
                            CO2.emissions..per.capita...latest.year) %>% 
   arrange(desc(CO2.emissions..latest.year)) %>% 
   slice_head(n=30)
+
+#The purpose of this chart is to compare the total CO2 emissions of a country in the latest year recorded to their CO2 emissions per capita.
+#This demonstrates how much a country emits CO2 compared to the size of the population, which is helpful in analyzing how well a country manages
+#emissions given its size. 
