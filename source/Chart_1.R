60316d1e5f22999d81c5d7e76ce63145bb8d1325
Pournami Varma <114948406+pournamivarma@users.noreply.github.com>
2022-12-05 00:39:31 -0800

    Update Chart_1.R

--- source/Chart_1.R
+++ source/Chart_1.R
@@ -9,26 +9,36 @@ library(ggplot2)
 data <- read_csv('https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-af/main/data/sustainability_scores.csv')
 View(data)
 
+# Chart 1 - Visualization of the sustainability scores for over 15K corporates.
+
+# loading ESG Ratings and Companies by Industry Data 
+library(tidyverse)
+library("dplyr")
+library("tidyr")
+library(ggplot2)
+
+data <- read_csv('https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-af/main/data/sustainability_scores.csv')
+View(data)
+
 data <- with(data, 
              data[!(Subsector == "" | is.na(Subsector)), ])
 
 # ESG Scores Dataframe 
 esg_scores <- data %>%
-  select(Subsector, Overall_ESG_SCORE, Environmental_SCORE, Social_SCORE, Governance_SCORE) %>%
-  filter((Overall_ESG_SCORE == 10)) %>%
-  slice_head(n=10) %>%
-  gather(key = ESG, value = scores, -Subsector) # all columns except 'Subsector' 
-  
+  select(Country, Sector, Subsector, Overall_ESG_SCORE) %>% 
+  slice_head(n=20)
+
 # Create a stacked bar chart of ESG Scores as compared by Subsector Industries
-chart_1 <- ggplot(esg_scores) +
-  geom_col(mapping = aes(x = Subsector, y = scores, fill = ESG)) +
+ggplot(esg_scores) +
+  geom_col(mapping = aes(x = Country, y = Overall_ESG_SCORE, fill = Subsector)) +
   labs(
-    title = "ESG Scores across Subsector Industries",
-    y = "Scores",
-    x = "Subsectors"
-  ) 
+    title = "Countries vs. ESG Scores based on Industry Sectors Globally",
+    y = "ESG Overall",
+    x = "Country"
+  ) +
+  guides(x = guide_axis(n.dodge = 2), y.sec = guide_axis())
   
-# Descriptive Paragraph:
+# Descriptive Paragraph [UPDATE W/ RELEVENT INFO]:
 # The purpose of this chart is to compare Environmental, Social and Governance (ESG) scores across a diverse set of subsector industries.
 # This takes into consideration environmental, social and governance scores independently and how their average which is documented by overall ESG scores,
 # is compared to the varying range of industry types which is helpful in understanding how ESG scores are impacted across multiple companies that are from 
