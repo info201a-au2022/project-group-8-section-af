a727f595702068bf37e4a1016b9600879706c7b5
maiab3 <114940989+maiab3@users.noreply.github.com>
2022-12-04 23:02:48 -0800

    Update server.R

--- source/server.R
+++ source/server.R
@@ -1,28 +1,93 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+data <- read.csv('https://raw.githubusercontent.com/info201b-2021-aut/final-project-MinHeo1/main/Data/survey.csv?token=AV5GCMIX2WD4DAFDLEJMEMLBXF55I')
+data <- data %>% filter(grepl('2014', data$Timestamp))
+data$Gender <- toupper(data$Gender)
+data$Gender[grep("\\bM\\b",data$Gender)] <- 'MALE'
+data$Gender[grep("\\bF\\b",data$Gender)] <- 'FEMALE'
+data$Gender[grep("\\bWOMAN\\b",data$Gender)] <- 'FEMALE'
+keepGender <- c("MALE", "FEMALE")
+data <- data[data$Gender %in% keepGender, ]
 
-    output$distPlot <- renderPlot({
+data_2 <- data %>%
+  select(state, no_employees, Age, Timestamp, treatment)%>%
+  filter(Age <= 100)%>%
+  group_by(state)%>%
+  group_by(no_employees)
+data_2 <- na.omit(data_2)
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+df <- 
+  data %>%
+  group_by(Country) %>%
+  filter(family_history == "No") %>%
+  filter(Country == "United States") %>% 
+  subset(select = c(Timestamp, Age, Gender, Country, state, family_history, no_employees, mental_vs_physical))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+mostCommon <-
+  df %>%
+  count(state, sort = TRUE)
 
-    })
+keepState <- as.vector(unlist(mostCommon$state))[1:10]
 
-})
+df <- df[df$state %in% keepState, ]
+
+server <- function(input, output) {
+#Page 2
+  
+  new_data_gender <- reactive({
+    data %>%
+      filter(Gender == input$radio)
+  })
+  
+  new_data_country <- reactive({
+    new_data_gender() %>% 
+    filter(Country == input$country)
+  })
+  
+  output$box <- renderPlot({
+    ggplot(new_data_country(), aes(x = no_employees)) +
+      labs(x = "number of employees", y = "count of surveyors") + 
+      geom_bar()
+  })
+  
+# Page 3
+  
+  new_data_treatment <- reactive({
+    data_2 %>%
+      filter(treatment == input$treatment, na.rm = TRUE)
+  })
+  
+  new_data_size <- reactive({
+    new_data_treatment() %>% 
+      filter(no_employees == input$size, na.rm = TRUE)
+  }) 
+  
+  
+  output$line <- renderPlot({
+    ggplot(new_data_size(), aes(y = Age, x= state,color = state)) +
+      labs(x = "States", y = "Age") + 
+      geom_boxplot()
+  })
+  
+# Page 4
+  
+  new_data_check <- reactive({
+    df %>%
+      filter(state == input$stateChoice, na.rm = TRUE)
+    
+  })
+  new_data_employee <- reactive({
+    new_data_check() %>% 
+      req(input$employees) %>%
+      filter(no_employees == input$employees, na.rm = TRUE)
+  })
+  
+  output$bar <- renderPlot({
+    ggplot(new_data_employee(), aes(x = no_employees)) +
+      geom_bar(aes(fill = state)) + facet_grid(. ~ state) + 
+      theme(axis.text.x=element_text(angle=90, hjust=1)) + labs(y="Mental Health Issues count", x="Number of Employees in Company")
+  })
+  
+}
