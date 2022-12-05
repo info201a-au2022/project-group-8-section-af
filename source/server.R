2f804e145a6d47ceffbb24553fbc01abb6634054
maiab3 <114940989+maiab3@users.noreply.github.com>
2022-12-04 23:13:31 -0800

    Update server.R

--- source/server.R
+++ source/server.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(dplyr)
 
 data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-af/main/data/sustainability_scores.csv")
+bar <-  read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-af/main/data/unsdg.csv")
 
 server <- function(input, output) {
   
